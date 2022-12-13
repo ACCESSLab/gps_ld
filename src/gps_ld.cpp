d14ca96276b95664a1e26269f94f7f0c9019a525
Tesfamichael Getahun <tesfamic@users.noreply.github.com>
2022-12-12 22:40:06 -0500

    Update gps_ld.cpp

--- src/gps_ld.cpp
+++ src/gps_ld.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <ros/ros.h>
+#include "detector.h"
 
+int main(int argc, char **argv){
+	ros::init(argc, argv, "gps_ld");
+
+	Detector lane;
+
+	ros::spin();
+	return 0;
+}
